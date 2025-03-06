6f4241d1ce9469c5fd6058e230918494e90c4038
Emily Mackay <35444424+mackayem@users.noreply.github.com>
2025-03-06 11:50:04 -0800

    added hero splash mtn image

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
     <section>
       <h1>Emily Mackay</h1>
       <h3>Front-End Developer</h3>
+      <Image
+        src='/images/hero-default-solid.png'
+        objectFit='cover'
+        fill={true}
+        className='w-full h-full object-cover rotate-180'
+      />
     </section>
   )
 }
