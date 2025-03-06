888308a6d2d70610b1957a222bd3befe75348ce7
Emily Mackay <35444424+mackayem@users.noreply.github.com>
2025-03-06 12:36:31 -0800

    adjusted spacing of headings

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ import Hero from "./sections/Hero";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="">
         <Hero/>
       </main>
       <footer className="">
--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -3,14 +3,17 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <section>
-      <h1>Emily Mackay</h1>
-      <h3>Front-End Developer</h3>
+    <section className='max-width-100'>
+      <div className='flex flex-col items-center m-30'>
+        <h1 className='text-6xl m-5'>Emily Mackay</h1>
+        <h3 className='text-2xl m-5'>Front-End Developer</h3>
+      </div>
       <Image
         src='/images/hero-default-solid.png'
         objectFit='cover'
         fill={true}
         className='w-full h-full object-cover rotate-180'
+        alt='mountain range with snow'
       />
     </section>
   )
