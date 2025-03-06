5ab445e4b8a140f72455867d7ef44b3e1b1e21d4
Emily Mackay <35444424+mackayem@users.noreply.github.com>
2025-03-06 13:06:58 -0800

    adjusted breakpoints after adding in dev text

--- src/app/sections/Hero.jsx
+++ src/app/sections/Hero.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Hero = () => {
   return (
     <section className='max-width-100'>
-      <div className='flex flex-col items-center mt-30 lg:m-30'>
+      <div className='flex flex-col items-center mt-20 lg:m-20'>
         <h1 className='text-4xl md:text-6xl m-5'>Emily Mackay</h1>
         <h3 className='text-xl md:text-2xl m-5 opacity-75'>Front-End Developer</h3>
         <p className="opacity-20 text-md md:text-lg mt-10">in development...</p>
